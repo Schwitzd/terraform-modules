63b01b562347d339ad9ef3df43cf8014331f2ff9
Schwitzd <15292575+Schwitzd@users.noreply.github.com>
2024-08-06 21:28:10 +0200

    ssh-key-pair added provider

---
+++ ssh-key-pair/provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.5"
+    }
+  }
+  required_version = ">= 0.13"
+}
