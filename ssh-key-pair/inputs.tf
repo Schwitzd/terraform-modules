91bbd373465eeb4224d79cc45c5c7cdfe7c5a6b9
Schwitzd <15292575+Schwitzd@users.noreply.github.com>
2024-08-06 21:23:13 +0200

    first release ssh-key-pair

---
+++ ssh-key-pair/inputs.tf
@@ -0,0 +1,46 @@
+variable "algorithm" {
+  description = "Name of the algorithm to use when generating the private key. Supported values are: RSA, ECDSA, ED25519."
+  type        = string
+  validation {
+    condition     = contains(["RSA", "ECDSA", "ED25519"], var.algorithm)
+    error_message = "The algorithm must be one of the following: RSA, ECDSA, ED25519."
+  }
+}
+
+variable "ecdsa_curve" {
+  description = "When algorithm is ECDSA, the name of the elliptic curve to use. Supported values are: P224, P256, P384, P521."
+  type        = string
+  default     = "P224"
+  validation {
+    condition     = var.algorithm != "ECDSA" || contains(["P224", "P256", "P384", "P521"], var.ecdsa_curve)
+    error_message = "The ecdsa_curve must be one of the following: P224, P256, P384, P521 when algorithm is ECDSA."
+  }
+}
+
+variable "rsa_bits" {
+  description = "When algorithm is RSA, the size of the generated RSA key, in bits."
+  type        = number
+  default     = 2048
+  validation {
+    condition     = var.algorithm != "RSA" || (var.rsa_bits >= 512 && var.rsa_bits <= 16384)
+    error_message = "The rsa_bits must be between 512 and 16384 when algorithm is RSA."
+  }
+}
+
+variable "write_to_file" {
+  description = "Whether to write the private key to a file."
+  type        = bool
+  default     = true
+}
+
+variable "keys_path" {
+  description = "The path where SSH keys will be stored."
+  type        = string
+  default     = "~/.ssh"
+}
+
+variable "keys_name" {
+  description = "The base name for the SSH key files."
+  type        = string
+}
+
---
+++ ssh-key-pair/main.tf
@@ -0,0 +1,25 @@
+resource "tls_private_key" "ssh_keys" {
+  algorithm = var.algorithm
+
+  # ECDSA curve, only set if algorithm is ECDSA
+  ecdsa_curve = var.algorithm == "ECDSA" ? var.ecdsa_curve : null
+
+  # RSA bits, only set if algorithm is RSA
+  rsa_bits = var.algorithm == "RSA" ? var.rsa_bits : null
+}
+
+resource "local_sensitive_file" "private_key" {
+  count = var.write_to_file ? 1 : 0
+
+  content         = tls_private_key.ssh_keys.private_key_openssh
+  filename        = local.private_key_filename
+  file_permission = "600"
+}
+
+resource "local_sensitive_file" "public_key" {
+  count = var.write_to_file ? 1 : 0
+
+  content         = tls_private_key.ssh_keys.public_key_openssh
+  filename        = local.public_key_filename
+  file_permission = "644"
+}
